3342ade7119b9d936d2d151c491c7ef1e6702c53
kael89-99 <131792774+kael89-99@users.noreply.github.com>
2025-11-30 20:13:10 +0000

    adding stuff

---
+++ mystatements.py
@@ -0,0 +1,10 @@
+my_list = ['apple', 'cherry', 'lemon']
+my_dict = {'drink':'coffe', 'milk':'whole'}
+
+for item in my_list:
+    print(f'my favorite thing to eat is {item}')
+
+for i,value in my_dict.items():
+    print(f'my favorite thing to drink is: {value}')
+
+my_dict = my_dict
